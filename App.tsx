795522af230a8c1576cce66d7d7d950fe153e948
leht377 <migithub2019@gmail.com>
2024-05-08 19:12:49 -0500

    typeScript agregado

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
+
 import { NavigationContainer } from '@react-navigation/native'
 import MyTabs from './src/components/MyTabs'
 
 export default function App () {
   return (
     <NavigationContainer>
-      <MyTabs />
-      {/* <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View> */}
+       <MyTabs />
     </NavigationContainer>
   )
 }
