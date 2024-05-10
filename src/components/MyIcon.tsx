80be2d905f8b9576c9da9e78ebec418ae2187d32
leht377 <migithub2019@gmail.com>
2024-05-10 08:48:36 -0500

    icono componente

--- src/components/MyIcon.tsx
+++ src/components/MyIcon.tsx
@@ -2,10 +2,13 @@ import { Ionicons } from '@expo/vector-icons'
 
 import React from 'react'
 
-const MyIcon = ({ nombre, color, tamano }) => {
-  return (
-    <Ionicons name={nombre} color={color} size={tamano} />
-  )
+interface Props {
+  nombre: any
+  color?: string
+  tamano?: number
+}
+const MyIcon: React.FC<Props> = ({ nombre, color, tamano }) => {
+  return <Ionicons name={nombre} color={color} size={tamano} />
 }
 
 export default MyIcon
